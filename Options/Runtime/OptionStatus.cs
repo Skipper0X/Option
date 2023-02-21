d8348f2bd8e3edd12cf205e1a914fc190c862170
Bhatti <usama.skipper0x@gmail.com>
2023-02-21 17:26:49 +0500

    - structure changes

---
+++ Options/Runtime/OptionStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Options.Runtime
+{
+	/// <summary>
+	/// <see cref="OptionStatus"/> defines the states of <see cref="Option{TReturn}"/>'s state for enum base checking...
+	/// </summary>
+	public enum OptionStatus
+	{
+		/// <summary>
+		/// <see cref="None"/> defines that option has no data wrapped in it...
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// <see cref="Success"/> mean data is wrapped successfully & ready to use...
+		/// </summary>
+		Success = 2,
+
+		/// <summary>
+		/// <see cref="Error"/> conveys some error is occured & data is can't be used & error message can be used for panics...
+		/// </summary>
+		Error = 4,
+	}
+}
